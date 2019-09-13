8a8f432f6e2f8aa36bdb3c72c2e5f7ba4f44d747
garybarnes19660630 <gary.barnes@exemail.com.au>
2019-09-13 22:24:45 +1000

    Fixed block colour.

--- custom.ts
+++ custom.ts
@@ -1,7 +1,7 @@
 // Extension to MakeCode to allow for inclusion of comments.
 
-//% weight=50 color=#0855AA icon="O" block="COMMENTS"
-namespace COMMENTS {
+//% weight=50 color=#FDD023 icon="O" block="Comments"
+namespace Comments {
 
     //% blockId="COMMENTS_LINE" block="Comment %x"
     export function comment_line(x: string) {

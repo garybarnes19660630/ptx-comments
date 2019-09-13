ef012a1d35fca3900910d3a9b2f9c094d7b748a7
garybarnes19660630 <gary.barnes@exemail.com.au>
2019-09-13 11:38:22 +1000

    Added custom.ts code for a line comment.

---
+++ custom.ts
@@ -0,0 +1,10 @@
+// Extension to MakeCode to allow for inclusion of comments.
+
+//% weight=50 color=#0855AA icon="O" block="COMMENTS"
+namespace COMMENTS {
+
+    //% blockId="COMMENTS_LINE" block="Comment %x"
+    export function comment_line(x: string) {
+    }
+
+}

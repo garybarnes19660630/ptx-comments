129b2ad813e6c3e9acfa8a2dbaab558deb3e66f0
garybarnes19660630 <gary.barnes@exemail.com.au>
2019-09-15 09:47:15 +1000

    Changed icon.

--- custom.ts
+++ custom.ts
@@ -1,7 +1,7 @@
 // Extension to MakeCode to allow for inclusion of comments.
 
-//% weight=50 color=#FDD023 icon="O" block="Comments"
-namespace Comments {
+//% weight=50 color=#FDD023 icon=#F075 block="Comments"
+namespace comments {
 
     //% blockId="COMMENTS_LINE" block="Comment %x"
     export function comment_line(x: string) {
